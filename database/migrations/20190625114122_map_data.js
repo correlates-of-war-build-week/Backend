c066991b3f0d966a6d1ab78eab41ecc58c3cab29
Shadowborn <james.starks023@gmail.com>
2019-06-25 14:41:46 -0600

    map data migration and seeds set up

---
+++ database/migrations/20190625114122_map_data.js
@@ -0,0 +1,23 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('map_data', map_data => {
+        map_data.increments();
+
+        map_data
+            .string('StateNme', 128)
+            .notNullable()
+            .unique();
+        map_data
+            .string('stateabb', 128)
+            .notNullable();
+        map_data
+            .integer('ccode', 128)
+            .notNullable();
+        map_data
+            .integer('pred_proba', 128)
+            .notNullable();
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('map_data');
+};
---
+++ mapdata/mapdata-model.js
@@ -0,0 +1,29 @@
+const db = require('../database/dbConfig.js');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+};
+
+function find() {
+  return db('map_data').select('id', 'StateNme');
+}
+
+function findBy(filter) {
+  return db('map_data').where(filter);
+}
+
+async function add(map_data) {
+  const [id] = await db('map_data').insert(map_data);
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db('map_data')
+    .select('id', 'StateNme')
+    .where({ id })
+    .first();
+}
