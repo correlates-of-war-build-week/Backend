661adac894c96a24e14e7bf49dc033e3d02b9fba
Shadowborn <james.starks023@gmail.com>
2019-06-26 20:41:27 -0600

    New seed data

--- mapdata/mapdata-model.js
+++ mapdata/mapdata-model.js
@@ -6,9 +6,25 @@ module.exports = {
   findBy,
   findById,
 };
+        
 
 function find() {
-  return db('map_data').select('id', 'StateNme', 'stateabb', 'ccode', 'pred_proba');
+  return db('map_data')
+    .select(
+      'id', 
+      "ccode",
+      "year",
+      "milper",
+      "irst",
+      "tpop",
+      "milex",
+      "exports",
+      "pred_proba",
+      "StateNme",
+      "country",
+      "latitude",
+      "longitude",
+    );
 }
 
 function findBy(filter) {
